65721ff8f9583a0c7e9d58bc59d8d35e973cfdde
ondreali <66229664+ondreali@users.noreply.github.com>
2020-06-11 23:28:58 -0500

    Add files via upload

---
+++ test_validation_with_try.py
@@ -0,0 +1,29 @@
+"""
+Program: test_validation_with_try.py
+Author:  Ondrea Li
+Date: 6/10/20
+
+The purpose of this program is to test validation_with_try.py
+"""
+
+import unittest
+import unittest.mock as mock
+from Topic4 import validation_with_try as vwt
+
+class MyTestCase(unittest.TestCase):
+#my class definitions
+    def test_average(self):
+         with mock.patch('builtins.input', side_effect=[85,90,95]):
+            assert vwt.average() == 90
+
+    def test_average_exception(self):
+         with self.assertRaises(ValueError):
+             vwt.average(-90, 89, 78)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
---
+++ validation_with_try.py
@@ -0,0 +1,47 @@
+"""
+Program: Coupon Calculation.py
+Author:  Ondrea Li
+Date: 6/10/20
+
+The purpose of this program is
+"""
+NUMBER_TEST = 1
+def average():
+    # Get input for scores
+    # declare variables, use score1, score2, score3 in calculation
+    return average_scores
+
+score1 = int(input("What is score1?"))
+score2 = int(input("What is score2?"))
+score3 = int(input("What is score3?"))
+
+def average(score1, score2, score3):
+    NUMBER_TESTS = 3
+    try:
+        if 0<score1<= 100:
+           print ("score is within range")
+        else:
+            print("score needs to be a positive integer")
+            raise ValueError
+        if 0<score2<= 100:
+            print("score is within range")
+        else:
+            print("score needs to be a positive integer")
+            raise ValueError
+        if 0<score3<= 100:
+            print("score is within range")
+        else:
+            print("score needs to be a positive integer")
+            raise ValueError
+    except:
+        print("count not convert")
+        raise ValueError
+
+    return float((score1 + score2 + score3)/NUMBER_TESTS)
+
+
+average_score = average(score1, score2, score3)
+print(average_score)
+
+if __name__ == '__main__':
+    pass
